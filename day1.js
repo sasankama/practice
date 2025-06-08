2c560dce9834c1df148ebc80088e48720ed7b359
sasanka <ssbkmlshn123@gmail.com>
2025-06-08 20:18:22 +0530

    my practice

---
+++ day1.js
@@ -0,0 +1,27 @@
+//day1
+/*let name = "sasanka"
+const age = 24;
+console.log("My name is "+ name + " and I am " + age + " years old");*/
+
+/*let name ="sasanka";
+let age =24;
+let isstudent =true;
+console.log(name + " is " + age + " years old " + isstudent);*/
+//day2
+/*let score = 50;
+ if (score >= 90){
+ console.log ("Exellent");
+ }
+ else if (score >= 70){
+ console.log ("good");
+ }
+ else if (score >= 50){
+ console.log ("pass");
+ }
+ else{
+    console.log ("fail");
+ }*/
+
+    //day3
+    let 
+ 
